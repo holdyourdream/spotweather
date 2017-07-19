0ab0263dac64ce61f3b99bae1d35c3d7c8f4c0fe
Edward <497546394@qq.com>
2017-07-19 10:06:30 +0800

    创建数据库和表的各项配置

--- app/src/main/java/com/spotweather/android/MainActivity.java
+++ app/src/main/java/com/spotweather/android/MainActivity.java
@@ -3,6 +3,8 @@ package com.spotweather.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.spotweather.android.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/spotweather/android/db/city.java
@@ -0,0 +1,50 @@
+package com.spotweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/*
+ * Created by 21737380 on 2017/7/19.
+ */
+
+public class city extends DataSupport {
+
+    private int id;
+
+    private  String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
